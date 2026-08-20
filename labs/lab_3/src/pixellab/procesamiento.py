07f1f65ac7de25c057e7f1a7df9fc5e1e092bde9
Agustín Zavala <agustin.zavala@ug.uchile.cl>
2026-08-20 17:31:09 -0400

    feat: agregar etapa 4

--- labs/lab_3/src/pixellab/procesamiento.py
+++ labs/lab_3/src/pixellab/procesamiento.py
@@ -12,22 +12,43 @@ class LibImagen:
     """Filtros y transformaciones que reciben y retornan ``Imagen``."""
 
     def to_negative(self, img_in: Imagen) -> Imagen:
-        # Su código aquí
-        raise NotImplementedError(
-            "Completen to_negative antes de ejecutar el programa."
-        )
+        img_out = 255 - img_in
+        img_out.imagen = img_out.imagen.astype(int)
+        return img_out
 
     def to_gray(self, img_in: Imagen) -> Imagen:
-        # Su código aquí
-        raise NotImplementedError(
-            "Completen to_gray antes de ejecutar el programa."
+        value_red_channel = img_in.imagen[..., :, 0]
+        value_green_channel = img_in.imagen[..., :, 1]
+        value_blue_channel = img_in.imagen[..., :, 2]
+        brightness = (
+            0.299 * value_red_channel
+            + 0.587 * value_green_channel
+            + 0.114 * value_blue_channel
         )
+        channel_brightness = np.stack(
+            (brightness, brightness, brightness), axis=-1
+        )
+        final_img = img_in.imagen.copy()
+        final_img[..., :, :] = channel_brightness
+        return Imagen(final_img.astype(int))
 
     def get_channel(self, img_in: Imagen, channel: str) -> Imagen:
-        # Su código aquí
-        raise NotImplementedError(
-            "Completen get_channel antes de ejecutar el programa."
-        )
+        is_channel_r = channel == "r"
+        is_channel_g = channel == "g"
+        is_channel_b = channel == "b"
+
+        if is_channel_r or is_channel_b or is_channel_g:
+            og_img = img_in.imagen.copy()
+            num_channel = 0 if is_channel_r else (1 if is_channel_g else 2)
+            indexes = [0, 1, 2]
+            indexes = [i for i in indexes if i != num_channel]
+            for i in indexes:
+                og_img[..., :, i] = np.zeros(og_img[..., :, i].shape)
+            return Imagen(og_img.astype(int))
+        else:
+            raise ValueError(
+                "Canal 'x' no válido. Valores posibles: 'r', 'g' o 'b'."
+            )
 
     def flip(self, img_in: Imagen, axis: str) -> Imagen:
         # Su código aquí
